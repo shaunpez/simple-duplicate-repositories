18f159cd9f83454c413f6b39559f99d6eae754b5
Shaun Pezeshki <web@hornblower.com>
2017-08-30 22:16:12 -0700

    Added 2 files to do an easy migration of repos

---
+++ bitbucket-to-github.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BASEDIR=`pwd -P`
+
+# Check if there are 4 arguments
+if [ "$#" -ne 4 ]; then
+  echo "Four arguments are required"
+  exit 1
+fi
+
+bitbucket_account=$1
+current_repo=$2
+github_account=$3
+new_repo=$4
+
+migrate_github_to_bitbucket() {
+  echo "** Attempting to Copying Github Repo to Bitbucket Repo **"
+  
+  # Start migrate process from github to bitbucket
+  git clone --mirror https://bitbucket.org/${bitbucket_account}/${current_repo}.git && cd ${current_repo}.git/ && git remote set-url --push origin https://github.com/${github_account}/${new_repo}.git && git push --mirror 2> /dev/null
+  
+  if [ $? -eq 0 ]; then
+    echo "** Bitbucket to Github migration successful **"
+  else
+    echo "** Bitbucket to Github migration failed **"    
+  fi
+}
+
+migrate_github_to_bitbucket
+
+# Clean Up Bitbucket Repo
+if [ -d "$BASEDIR/$current_repo.git" ]; then
+    printf '%s\n' "** Removing Repo Directory **"
+    rm -rf ${BASEDIR}/${current_repo}.git/
+fi
